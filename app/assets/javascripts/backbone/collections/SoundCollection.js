e425c80c120ada0f3758fe6d3be2bef91c18f066
theerickramer <theerickramer@gmail.com>
2014-10-30 01:40:49 -0400

    fixed user id for collection

--- app/assets/javascripts/backbone/collections/SoundCollection.js
+++ app/assets/javascripts/backbone/collections/SoundCollection.js
@@ -2,5 +2,5 @@ var RecloudApp = RecloudApp || { Models: {}, Views: {}, Collections: {}, Routers
 
 RecloudApp.Collections.Sound = Backbone.Collection.extend({
 	model: RecloudApp.Models.Sound,
-	url: '/users/1/sounds'
+	url: '/users/' + $('.user').attr('id') + '/sounds'
 });
